41321accc9622d4dfdeeef4fef84039017de0956
zelmon64 <zelmon64@gmail.com>
2018-10-27 23:05:12 +0100

    Use the message bus to trigger the finished booting indication
    The message type "mycroft.skills.initialized" is sent when all
    skills have been loaded at startup. Jarbas_Ai suggested this much
    cleaner method of detection.

--- __init__.py
+++ __init__.py
@@ -23,12 +23,14 @@ class zzzFinishedBootingSkill(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(zzzFinishedBootingSkill, self).__init__(name="zzzFinishedBootingSkill")
-        
+    
     def initialize(self):
-        # self.speak('started')
+        self.add_event("mycroft.skills.initialized", self.handle_boot_finished)
+        LOG.debug('add event handle boot finished')
+    
+    def handle_boot_finished(self):
         self.speak_dialog('finished.booting')
-        # self.speak_dialog('finished.booting', expect_response=True)
-        LOG.info('finished booting')
+        LOG.debug('finished booting')
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
