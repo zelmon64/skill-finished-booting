85f1f9cf1ea7b3078d23ca33fe9f78b3ffd4489e
zelmon64 <zelmon64@gmail.com>
2018-10-27 19:37:56 +0100

    Initial commit
    Used the 00__skill_template.

---
+++ README.md
@@ -0,0 +1,13 @@
+## ZZZ Finished Booting Skill
+Skill to determine when Mycroft AI has finished booting up.
+
+## Description 
+A more verbose description, including any extra instructions or
+information that didn't fit in the one line.
+Mycroft loads the skills alphabetically (apart from the prioritized
+skills) so this skill is named such that it should always be the last
+skill to load. When this skill loads it assumes it is the last skill
+to load and announces that Mycroft has finished booting up.
+
+## Credits 
+zelmon64
---
+++ __init__.py
@@ -0,0 +1,36 @@
+#~ zzz-finished-booting-skill - A simple skill to know when mycroft finishes booting up.
+#~ Copyright (C) 2018  zelmon64
+#~ Github - https://github.com/zelmon64
+
+#~ This program is free software: you can redistribute it and/or modify
+#~ it under the terms of the GNU General Public License as published by
+#~ the Free Software Foundation, either version 3 of the License, or
+#~ (at your option) any later version.
+
+#~ This program is distributed in the hope that it will be useful,
+#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#~ GNU General Public License for more details.
+
+#~ You should have received a copy of the GNU General Public License
+#~ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from mycroft import MycroftSkill
+from mycroft.util.log import LOG
+
+class zzzFinishedBootingSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(zzzFinishedBootingSkill, self).__init__(name="zzzFinishedBootingSkill")
+        
+    def initialize(self):
+        # self.speak('started')
+        self.speak_dialog('finished.booting')
+        # self.speak_dialog('finished.booting', expect_response=True)
+        LOG.info('finished booting')
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return zzzFinishedBootingSkill()
